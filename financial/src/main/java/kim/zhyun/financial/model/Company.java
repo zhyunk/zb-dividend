60ff484ee3a093f411bb8cf9694e066a5b93c200
zhyunk <gimwlgus@gmail.com>
2023-05-31 21:03:40 +0900

    model 클래스 정의

---
+++ financial/src/main/java/kim/zhyun/financial/model/Company.java
@@ -0,0 +1,12 @@
+package kim.zhyun.financial.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Company {
+
+    private String ticker;
+    private String name;
+}
