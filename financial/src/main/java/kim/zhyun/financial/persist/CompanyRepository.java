315a4fa5503cb6e128c68e239f661972c35d3dfb
zhyunk <gimwlgus@gmail.com>
2023-06-01 23:55:35 +0900

    배당금 조회

--- financial/src/main/java/kim/zhyun/financial/persist/CompanyRepository.java
+++ financial/src/main/java/kim/zhyun/financial/persist/CompanyRepository.java
@@ -4,7 +4,11 @@ import kim.zhyun.financial.persist.entity.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     boolean existsByTicker(String ticker);
+
+    Optional<CompanyEntity> findByName(String name);
 }
--- financial/src/main/java/kim/zhyun/financial/persist/DividendRepository.java
+++ financial/src/main/java/kim/zhyun/financial/persist/DividendRepository.java
@@ -4,6 +4,9 @@ import kim.zhyun.financial.persist.entity.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+    List<DividendEntity> findAllByCompanyId(Long companyId);
 }
