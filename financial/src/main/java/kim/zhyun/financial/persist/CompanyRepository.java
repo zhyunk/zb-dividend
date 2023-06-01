82086f4eb7db7c94d623f1f04d93816b0e3832ef
zhyunk <gimwlgus@gmail.com>
2023-06-02 00:38:19 +0900

    sql like절을 사용한 auto complete 구현

--- financial/src/main/java/kim/zhyun/financial/persist/CompanyRepository.java
+++ financial/src/main/java/kim/zhyun/financial/persist/CompanyRepository.java
@@ -1,9 +1,12 @@
 package kim.zhyun.financial.persist;
 
 import kim.zhyun.financial.persist.entity.CompanyEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,6 @@ public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     boolean existsByTicker(String ticker);
 
     Optional<CompanyEntity> findByName(String name);
+
+    Page<CompanyEntity> findByNameStartingWithIgnoreCase(String s, Pageable limit);
 }
